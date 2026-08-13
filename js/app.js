59545fa2c21f9b25533877b795f4b7d3411381d9
beabae86-code <masengobea86@gmail.com>
2026-08-13 07:20:44 +0200

    fixed firebase

--- js/app.js
+++ js/app.js
@@ -1,38 +1,53 @@
-// import { auth } from "../firebase.js";
-// import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { auth } from "../firebase.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { db } from "../firebase.js";
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
-// const navbar = document.querySelector("#navbar");
-// const footer = document.querySelector("footer");
+console.log("Firebase connected:", db);
 
-// // function renderNavbar(user) {
-// //   navbar.innerHTML = `
-// //     <nav class="navbar">
-// //       <a class="brand" href="index.html">URBAN<span>THREADS</span></a>
-// //       <div class="nav-links">
-// //         <a href="shop.html">Shop</a>
-// //         <a href="cart.html">Cart</a>
-//         ${
-//           user
-//             ? `<span class="user-email">${user.email}</span>
-//                <button id="logoutBtn" class="nav-button">Log out</button>`
-//             : `<a href="login.html" class="nav-button">Log in</a>`
-//         }
-//   //     </div>
-//   //   </nav>
-//   // `;
+const productsRef = collection(db, "products");
 
-//   document.querySelector("#logoutBtn")?.addEventListener("click", async () => {
-//     await signOut(auth);
-//     window.location.href = "index.html";
-//   });
-// }
+const snapshot = await getDocs(productsRef);
 
-// footer.innerHTML = `
-//   // <div class="footer-inner">
-//   //   <strong>URBAN THREADS</strong>
-//   //   <p>Streetwear for everyday life.</p>
-//   //   <small>© 2026 Urban Threads</small>
-//   // </div>
-// `;
+snapshot.forEach((doc) => {
+  console.log("PRODUCT:", doc.id, doc.data());
+});
 
-// onAuthStateChanged(auth, renderNavbar);
+const navbar = document.querySelector("#navbar");
+const footer = document.querySelector("footer");
+
+function renderNavbar(user) {
+  navbar.innerHTML = `
+    <nav class="navbar">
+      <a class="brand" href="index.html">URBAN<span>THREADS</span></a>
+      <div class="nav-links">
+        <a href="shop.html">Shop</a>
+        <a href="cart.html">Cart</a>
+        ${
+          user
+            ? `<span class="user-email">${user.email}</span>
+               <button id="logoutBtn" class="nav-button">Log out</button>`
+            : `<a href="login.html" class="nav-button">Log in</a>`
+        }
+  //     </div>
+  //   </nav>
+  // `;
+
+  document.querySelector("#logoutBtn")?.addEventListener("click", async () => {
+    await signOut(auth);
+    window.location.href = "index.html";
+  });
+}
+
+footer.innerHTML = `
+  // <div class="footer-inner">
+  //   <strong>URBAN THREADS</strong>
+  //   <p>Streetwear for everyday life.</p>
+  //   <small>© 2026 Urban Threads</small>
+  // </div>
+`;
+
+onAuthStateChanged(auth, renderNavbar);
